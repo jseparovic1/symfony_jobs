63c98b585cece5987d5ac56a5ced3a939bf06003
Jurica Šeparović <jsepar00@fesb.hr>
2018-03-04 13:39:48 +0100

    Add pagination components

--- src/Controller/Job/ShowJobsAction.php
+++ src/Controller/Job/ShowJobsAction.php
@@ -4,19 +4,51 @@ namespace App\Controller\Job;
 
 use App\Controller\BaseAction;
 use App\Factory\JobViewFactory;
-use App\Repository\CompanyRepository;
+use App\Factory\PageViewFactory;
 use App\Repository\JobRepository;
-use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
+use App\Util\PaginatorDetails;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Component\HttpFoundation\Request;
 
 class ShowJobsAction extends BaseAction
 {
-    public function __invoke(JobRepository $jobRepository, JobViewFactory $jobViewFactory, UploaderHelper $helper)
-    {
-        $jobs = $jobRepository->findAll();
+    /**
+     * @var PageViewFactory
+     */
+    private $pageViewFactory;
 
-        foreach ($jobs as $job) {
-            $data[] = $jobViewFactory->create($job);
+    public function __construct(PageViewFactory $pageViewFactory)
+    {
+        $this->pageViewFactory = $pageViewFactory;
+    }
+
+    /**
+     * @param JobRepository $jobRepository
+     * @param JobViewFactory $jobViewFactory
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Doctrine\ORM\Query\QueryException
+     */
+    public function __invoke(JobRepository $jobRepository, JobViewFactory $jobViewFactory, Request $request)
+    {
+        $search = $request->get('search');
+        $remote = $request->get('remote');
+
+        $paginatorDetails = new PaginatorDetails($request->attributes->get('_route'), $request->query->all());
+        $queryBuilder = $jobRepository->createJobListQueryBuilder($search, $remote);
+
+        $pagerfanta = new Pagerfanta(new DoctrineORMAdapter($queryBuilder));
+
+        $pagerfanta->setMaxPerPage($paginatorDetails->limit());
+        $pagerfanta->setCurrentPage($paginatorDetails->page());
+
+        $pageView = $this->pageViewFactory->create($pagerfanta, $paginatorDetails->route(), $paginatorDetails->parameters());
+
+        foreach ($pagerfanta->getCurrentPageResults() as $currentPageResult) {
+            $pageView->items[] = $jobViewFactory->create($currentPageResult);
         }
-        return $this->createView($data);
+
+        return $this->createView($pageView);
     }
 }
--- src/Repository/JobRepository.php
+++ src/Repository/JobRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Job;
+use Doctrine\Common\Collections\Criteria;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class JobRepository extends BaseRepository
@@ -14,5 +15,27 @@ class JobRepository extends BaseRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Job::class);
+    }
+
+
+    /**
+     * @param $search
+     * @param null $remote
+     * @return \Doctrine\ORM\QueryBuilder
+     * @throws \Doctrine\ORM\Query\QueryException
+     */
+    public function createJobListQueryBuilder($search, $remote = null)
+    {
+        $queryBuilder = $this->createQueryBuilder('job')
+            ->andWhere('job.search LIKE :location OR job.title LIKE :search')
+            ->setParameter('search', '%'.$search.'%')
+            ->orderBy('job.createdAt');
+
+        if ($remote) {
+            $remoteCriteria = Criteria::create()->where(Criteria::expr()->eq("remote", true));
+            $queryBuilder->addCriteria($remoteCriteria);
+        }
+
+        return $queryBuilder;
     }
 }
