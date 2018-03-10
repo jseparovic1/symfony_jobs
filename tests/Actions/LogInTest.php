9691b7d2c2a051c4421fe4c4f014d975ea31fb32
Jurica Šeparović <jsepar00@fesb.hr>
2018-03-11 00:43:46 +0100

    Fuking fixtures setup ! woohohohoh

--- tests/Actions/LogInTest.php
+++ tests/Actions/LogInTest.php
@@ -3,24 +3,20 @@
 namespace App\Tests\Actions;
 
 use App\Util\JsonApiTestCase;
-use Symfony\Component\HttpFoundation\Response;
 
 class LogInTest extends JsonApiTestCase
 {
-//    public static function createKernel(array $options = [])
-//    {
-//        return new Kernel('test', true);
-//    }
-
     /**
      * @test
      */
     public function it_returns_token_when_user_logs_in()
     {
-        $this->loadFixturesFromFile('user.yml');
+        $this->loadFixtures('user', 'company');
 
-        $response = $this->sendJsonRequest('GET', '/api/login_check', ['_username' => 'matko' , '_password' => 'pero']);
+        $this->client->request('GET', '/api/jobs');
 
-        $this->assertResponseCode($response, Response::HTTP_BAD_REQUEST);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'hi');
     }
 }
